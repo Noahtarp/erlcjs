77e5f85867f11ca4e666c507cac961282c449ecd
Noinkin <87577662+Noinkin@users.noreply.github.com>
2026-07-03 13:42:07 +0100

    feat: Dedicated Error Classes

--- packages/core/src/errors/index.ts
+++ packages/core/src/errors/index.ts
@@ -3,12 +3,52 @@
  * @public
  */
 export class InvalidServerKeyError extends Error {
-    /**
-     * Creates an instance of InvalidServerKeyError.
-     * @param message - The error message. Defaults to 'Invalid Server API Key.'.
-     */
     constructor(message: string = 'Invalid Server API Key.') {
         super(message);
         this.name = 'InvalidServerKeyError';
+    }
+}
+
+/**
+ * Error thrown when an invalid ER:LC Global API Key is provided.
+ * @public
+ */
+export class InvalidGlobalKeyError extends Error {
+    constructor(message: string = 'Invalid Global API Key.') {
+        super(message);
+        this.name = 'InvalidGlobalKeyError';
+    }
+}
+
+/**
+ * Error thrown when a custom in-game command is invalid.
+ * @public
+ */
+export class CustomCommandError extends Error {
+    constructor(message: string = 'Invalid Custom In-Game Command.') {
+        super(message);
+        this.name = 'CustomCommandError';
+    }
+}
+
+/**
+ * Error thrown when a timeout occurs.
+ * @public
+ */
+export class TimeoutError extends Error {
+    constructor(message: string = 'Timeout.') {
+        super(message);
+        this.name = 'TimeoutError';
+    }
+}
+
+/**
+ * Error thrown when the ER:LC API returns an error.
+ * @public
+ */
+export class ERLCAPIError extends Error {
+    constructor(message: string = 'ER:LC API Error.') {
+        super(message);
+        this.name = 'ERLCAPIError';
     }
 }
