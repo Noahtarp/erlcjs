de1858afe37585866563929bffa492050d21f3fd
Noinkin <87577662+Noinkin@users.noreply.github.com>
2026-07-03 22:05:47 +0100

    docs: Presets Getting Started

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -30,6 +30,12 @@ export default defineConfig({
 						{ label: 'Server & Command Management', slug: 'guides/server-commands' },
 						{ label: 'Handling Events', slug: 'guides/events' },
                         { label: 'Accessing the Cache', slug: 'guides/cache' },
+                        { 
+                            label: 'Using Presets', 
+                            items: [
+                                { label: 'Getting Started', slug: 'guides/presets/getting-started' },
+                            ]
+                        },
 					],
 				},
 				{
